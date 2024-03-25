c006655e379ce894ae80d75f54612519d3fa6414
abhijeetjohnkujur <abhijeetkujur000@gmail.com>
2024-03-25 16:33:06 +0530

    Added front layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./views/**/*.{html,js,ejs}"],
+  content: ["./views/**/*.{html,js,ejs}",
+  "./*.{html,js}"],
   theme: {
     extend: {},
   },
