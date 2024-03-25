e3b017e699b4d8cffa57cd4003c25cb7a7dab776
abhijeetjohnkujur <abhijeetkujur000@gmail.com>
2024-03-25 17:01:08 +0530

    Added code for data entry in database

--- controllers/homecontroller.js
+++ controllers/homecontroller.js
@@ -1,5 +1,23 @@
+const todoList = require('../models/todo')
+
 module.exports.home = (req,res) => {
     return res.render('home', {
         title: "TaskMate | TodoList App"
+    })
+}
+
+module.exports.addTodo = (req,res) => {
+    todoList.create({
+        task: req.body.task,
+        category: req.body.category,
+        duedate: req.body.duedate
+    })
+    .then(result => {
+        console.log(result)
+        return res.redirect('back')
+    })
+    .catch(err => {
+        console.log(err)
+        return res.redirect('back')
     })
 }
--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,10 @@ const router = express.Router()
 //controllers
 const homecontroller = require('../controllers/homecontroller')
 
-// Home Page
+// Home Page Route
 router.get('/', homecontroller.home)
+
+// Add Task Route
+router.post('/add-todo', homecontroller.addTodo)
 
 module.exports = router
